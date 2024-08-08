89975b08c3fc736f7de886624e46d211ec762391
Daniel Lamas <dlamas1298@gmail.com>
2024-08-08 14:55:52 -0300

    🔧 chore: formatting code

--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,40 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { Logger, ValidationPipe } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { ConfigService } from '@nestjs/config';
-import { corsConfig } from './config';
+import { Logger, ValidationPipe } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { AppModule } from './app.module'
+import { corsConfig } from './config'
 
 async function bootstrap() {
-  const logger = new Logger('Bootstrap');
+	const logger = new Logger('Bootstrap')
 
-  const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create(AppModule)
 
-  app.setGlobalPrefix('api');
+	app.setGlobalPrefix('api')
 
-  app.enableCors(corsConfig);
+	app.enableCors(corsConfig)
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-    }),
-  );
+	app.useGlobalPipes(
+		new ValidationPipe({
+			whitelist: true,
+			forbidNonWhitelisted: true,
+		})
+	)
 
-  const config = new DocumentBuilder()
-    .setTitle('JOURNEYS API')
-    .setDescription('Aplicación para la optimización de viajes y rutas')
-    .setVersion('1.0')
-    .build();
+	const config = new DocumentBuilder()
+		.setTitle('JOURNEYS API')
+		.setDescription('Aplicación para la optimización de viajes y rutas')
+		.setVersion('1.0')
+		.build()
 
-  const document = SwaggerModule.createDocument(app, config);
+	const document = SwaggerModule.createDocument(app, config)
 
-  SwaggerModule.setup('api', app, document);
+	SwaggerModule.setup('api', app, document)
 
-  const port = app.get(ConfigService).get('PORT');
+	const port = app.get(ConfigService).get('PORT')
 
-  await app.listen(port);
+	await app.listen(port)
 
-  logger.log(`Application is running on port ${port}`);
+	logger.log(`Application is running on port ${port}`)
 }
-bootstrap();
+bootstrap()
