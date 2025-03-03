649b837250b3a1b1c44cf787bb7b935e80c10571
Daniel Lamas <dlamas1298@gmail.com>
2025-03-02 22:53:02 -0300

    feat: update UpdateTripDto to extend PartialType for better flexibility in trip updates

--- src/modules/trips/dtos/update-trip.dto.ts
+++ src/modules/trips/dtos/update-trip.dto.ts
@@ -1,3 +1,4 @@
+import { PartialType } from '@nestjs/swagger'
 import { CreateTripDto } from './create-trip.dto'
 
-export class UpdateTripDto extends CreateTripDto {}
+export class UpdateTripDto extends PartialType(CreateTripDto) {}
